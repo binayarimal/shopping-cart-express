23aa223c076801541968997816acd1b19f1308a3
Binaya Rimal <rimal.binaya@gmail.com>
2019-06-30 18:17:48 -0700

    heroku

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -5,14 +5,14 @@ const passportConfig = require("./passport-config");
 const session = require("express-session");
 const expressValidator = require("express-validator");
 const viewsFolder = path.join(__dirname, "..", "public");
-const build = path.join(__dirname, "..", "client/build");
+
 
  module.exports = {
   init(app, express){
 
     app.set("views", viewsFolder);
     app.set("view engine", "ejs");
-    app.use(express.static(build));
+  app.use(express.static(path.join(__dirname,"..", 'client/build')))
     app.use(bodyParser.json());
     app.use(session({
    secret: process.env.cookieSecret,
@@ -22,6 +22,9 @@ const build = path.join(__dirname, "..", "client/build");
  }));
    passportConfig.init(app);
    app.use(expressValidator());
+   app.get('*', (req,res) =>{
+       res.sendFile(path.join(__dirname+'/client/build/index.html'));
+   });
 
 }
 }
