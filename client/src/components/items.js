34780a1f630945bedc23fb3f820f722062c710b6
Binaya Rimal <rimal.binaya@gmail.com>
2019-07-02 20:48:31 -0700

    socket works successfully

--- client/src/components/items.js
+++ client/src/components/items.js
@@ -9,7 +9,7 @@ class Items extends Component {
       item :"",
       itemList:[],
       email:"",
-      endpoint : "https://binayarimal-shopping-cart.herokuapp.com"
+      endpoint : "http://localhost:5000"
     }
   }
  setItemList(items){
--- src/server.js
+++ src/server.js
@@ -14,62 +14,56 @@ io.on('connection', socket => {
   socket.on('give items', (id) => {
     itemQueries.getAllitems(id, (err, items)=>{
       if(err){
-      socket.emit('err', err)
+        socket.emit('err', err)
       } else {
-      socket.emit('show items', items)
+        io.emit('show items', items)
       }
     })
   });
   socket.on('post items', (data) => {
-      let newItem = {
-        title: data.item,
-        shopListId:data.shopListId
-      };
-      itemQueries.addItem(newItem, (err, item) => {
-        if(err){
-          console.log(err)
-        } else {
-          const message ="success";
-          socket.emit('success',message )
-        }
-      })
-
+    let newItem = {
+      title: data.item,
+      shopListId:data.shopListId
+    };
+    itemQueries.addItem(newItem, (err, item) => {
+      if(err){
+        console.log(err)
+      } else {
+        const message ="success";
+        io.emit('success',message )
+      }
     })
+
+  })
 
 
   socket.on('disconnect', () => {
     console.log('user disconnected')
   })
 });
-
-
 app.use(history());
- app.set("port", port);
- server.listen(port);
-
-
-
- if(process.env.NODE_ENV === 'production'){
-app.use(express.static( "client/build"));
-app.get('/', (req,res) =>{
+app.set("port", port);
+server.listen(port);
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static( "client/build"));
+  app.get('/', (req,res) =>{
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
+  });
 }
 
-
- function normalizePort(val) {
-   const port = parseInt(val, 10);
-   if (isNaN(port)) {
-     return val;
-   }
-   if (port >= 0) {
-     return port;
-   }
-   return false;
- }
+function normalizePort(val) {
+  const port = parseInt(val, 10);
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+}
 
 
 
 server.on("listening", () => {
   console.log(`server is listening for requests on port ${server.address().port}`);
-   });
+});
